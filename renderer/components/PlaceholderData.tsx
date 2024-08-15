52d1d9f81ee85b8bd842a79703756c5a48115ea5
J. <77263040+jarvilehto@users.noreply.github.com>
2024-08-15 15:39:36 +0300

    WIP: gameBoard layout

---
+++ renderer/components/PlaceholderData.tsx
@@ -0,0 +1,81 @@
+const PlaceholderData = [
+  {
+    id: 0,
+    name: "Placeholder 1",
+    questions: [
+      { points: 100, question: "Lorem", answer: "ipsum" },
+      { points: 200, question: "lorem", answer: "ipsum" },
+      { points: 300, question: "lorem", answer: "ipsum" },
+      { points: 400, question: "lorem", answer: "ipsum" },
+      { points: 500, question: "lorem", answer: "ipsum" },
+    ],
+  },
+  {
+    id: 1,
+    name: "Placeholder 1",
+    questions: [
+      { points: 100, question: "Lorem", answer: "ipsum" },
+      { points: 200, question: "lorem", answer: "ipsum" },
+      { points: 300, question: "lorem", answer: "ipsum" },
+      { points: 400, question: "lorem", answer: "ipsum" },
+      { points: 500, question: "lorem", answer: "ipsum" },
+    ],
+  },
+  {
+    id: 2,
+    name: "Placeholder 1",
+    questions: [
+      { points: 100, question: "Lorem", answer: "ipsum" },
+      { points: 200, question: "lorem", answer: "ipsum" },
+      { points: 300, question: "lorem", answer: "ipsum" },
+      { points: 400, question: "lorem", answer: "ipsum" },
+      { points: 500, question: "lorem", answer: "ipsum" },
+    ],
+  },
+  {
+    id: 3,
+    name: "Placeholder 1",
+    questions: [
+      { points: 100, question: "Lorem", answer: "ipsum" },
+      { points: 200, question: "lorem", answer: "ipsum" },
+      { points: 300, question: "lorem", answer: "ipsum" },
+      { points: 400, question: "lorem", answer: "ipsum" },
+      { points: 500, question: "lorem", answer: "ipsum" },
+    ],
+  },
+  {
+    id: 4,
+    name: "Placeholder 1",
+    questions: [
+      { points: 100, question: "Lorem", answer: "ipsum" },
+      { points: 200, question: "lorem", answer: "ipsum" },
+      { points: 300, question: "lorem", answer: "ipsum" },
+      { points: 400, question: "lorem", answer: "ipsum" },
+      { points: 500, question: "lorem", answer: "ipsum" },
+    ],
+  },
+  {
+    id: 5,
+    name: "Placeholder 1",
+    questions: [
+      { points: 100, question: "Lorem", answer: "ipsum" },
+      { points: 200, question: "lorem", answer: "ipsum" },
+      { points: 300, question: "lorem", answer: "ipsum" },
+      { points: 400, question: "lorem", answer: "ipsum" },
+      { points: 500, question: "lorem", answer: "ipsum" },
+    ],
+  },
+  {
+    id: 6,
+    name: "Placeholder 1",
+    questions: [
+      { points: 100, question: "Lorem", answer: "ipsum" },
+      { points: 200, question: "lorem", answer: "ipsum" },
+      { points: 300, question: "lorem", answer: "ipsum" },
+      { points: 400, question: "lorem", answer: "ipsum" },
+      { points: 500, question: "lorem", answer: "ipsum" },
+    ],
+  },
+];
+
+export default PlaceholderData;
--- renderer/pages/gameBoard.tsx
+++ renderer/pages/gameBoard.tsx
@@ -6,11 +6,88 @@ TODO: Contestants: names + points
 
 */
 
+import { useState } from "react";
+import PlaceholderData from "../components/PlaceholderData";
+
+const questionCard = () => {};
+const answeredQuestionCard = () => {};
+
+const openQuestion = (porps) => {};
+
 export default function gameBoard() {
+  const [categories, setCategories] = useState();
+  const [openQuestion, setOpenQuestion] = useState([]);
+  const [contestants, setContestants] = useState([]);
+  const placeholderQuestions = PlaceholderData;
+
+  // USE this + concat to add contestants
+  const contestant = {
+    name: "",
+    points: "",
+  };
+
+  const saveGameState = () => {};
+
   return (
-    <div>
+    <div className="h-full w-full">
+      <div className="hidden">Open Question</div>
       <div id="Header"></div>
-      <div id="Game"></div>
+      <div id="Game" className="flex flex-row justify-center">
+        <div key={placeholderQuestions[0].id} className="m-4">
+          <h2>{placeholderQuestions[0].name}</h2>
+          {placeholderQuestions[0].questions.map((q, i) => (
+            <div key={i}>{q.question}</div>
+          ))}
+        </div>
+        <div className="m-4">
+          <h2>Category name</h2>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+        </div>
+        <div className="m-4">
+          <h2>Category name</h2>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+        </div>
+        <div className="m-4">
+          <h2>Category name</h2>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+        </div>
+        <div className="m-4">
+          <h2>Category name</h2>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+        </div>
+        <div className="m-4">
+          <h2>Category name</h2>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+        </div>
+        <div className="m-4">
+          <h2>Category name</h2>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+        </div>
+      </div>
       <div id="Contestants"></div>
     </div>
   );
