f78e08548116e6ce44c884de2129e2a112d4ee49
J. <77263040+jarvilehto@users.noreply.github.com>
2024-10-04 18:10:26 +0300

    added embed support for question cardsgit add .git add .

--- renderer/components/OpenQuestion.tsx
+++ renderer/components/OpenQuestion.tsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
 
+/*
+
+TODO: MP3 UPLOAD
+
+*/
+
 const OpenQuestion = (props) => {
   const { categories, openQuestion } = props;
 
@@ -9,7 +15,33 @@ const OpenQuestion = (props) => {
     ({ points }) => points === openQuestion.points
   );
 
+  /*
+    Use this to test 
+    https://www.youtube.com/watch?v=W7MrDt_NPFk normal youtube link 
+    https://www.youtube.com/embed/W7MrDt_NPFk
+
+    "https://streamable.com/e/hodezr" <- e means embed 
+
+src="https://www.youtube.com/embed/W7MrDt_NPFk
+
+
+  
+  */
+  const DetermineEmbed = () => {
+    let test = CurrentQuestion.question.includes("youtube");
+    if (test) {
+      const searchURL = new URL(CurrentQuestion.question).searchParams;
+      let tester = searchURL.get("v");
+      return `https://www.youtube.com/embed/${tester}`;
+    } else {
+      const searchURL = new URL(CurrentQuestion.question);
+      let tester = searchURL.pathname.substring(1);
+      return `https://streamable.com/e/${tester}`;
+    }
+  };
+
   console.log("CurrentQuestion", CurrentQuestion);
+  DetermineEmbed();
 
   const [showAnswer, setShowAnswer] = useState(false);
 
@@ -19,8 +51,31 @@ const OpenQuestion = (props) => {
         {!showAnswer && (
           <>
             <h1 className="text-6xl" style={{ color: "black" }}>
-              {CurrentQuestion.question}
+              {/*CurrentQuestion.question*/}
             </h1>
+            <div
+              style={{
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                paddingBottom: "56,250%",
+              }}
+            >
+              <iframe
+                height="100%"
+                src={DetermineEmbed()}
+                width="100%"
+                style={{
+                  border: "none",
+                  width: "100",
+                  height: "100%",
+                  position: "absolute",
+                  left: "0px",
+                  top: "0px",
+                  overflow: "hidden",
+                }}
+              ></iframe>
+            </div>
             <div className="absolute bottom-0 right-2 text-4xl p-2">
               <button onClick={() => setShowAnswer(!showAnswer)}>✅</button>
               <button>❌</button>
